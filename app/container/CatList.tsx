d788316b51690e98be61fa3da625a5a30900866c
Mel Carlo <melcarlo.iguis@gmail.com>
2026-03-01 21:35:38 +0800

    fix: remove all usage of router.push and use Link component instead for prefetching

--- app/container/CatList.tsx
+++ app/container/CatList.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ICat } from "@/models/Cats";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { FC } from "react";
 import CatCard from "./CatCard";
 
@@ -10,26 +10,24 @@ interface CatListProps {
 }
 
 const CatList: FC<CatListProps> = ({ cats }) => {
-  const router = useRouter();
 
   return (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 p-6">
       {cats.map((cat: any, index: number) => (
-        <motion.div
-          key={cat._id}
-          initial={{ opacity: 0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.5,
-            delay: index * 0.1,
-          }}
-          viewport={{ once: true }}
-          onClick={() => {
-            router.push(`/cats/view/${cat._id}`);
-          }}
-        >
-          <CatCard {...cat} />
-        </motion.div>
+        <Link href={`/cats/view/${cat._id}`}>
+          <motion.div
+            key={cat._id}
+            initial={{ opacity: 0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.5,
+              delay: index * 0.1,
+            }}
+            viewport={{ once: true }}
+          >
+            <CatCard {...cat} />
+          </motion.div>
+        </Link>
       ))}
     </div>
   );
